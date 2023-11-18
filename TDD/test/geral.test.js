6b66931afa05f0b271abb780212e6c9d3dd44760
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-11-18 09:27:52 -0400

    ee

--- TDD/test/geral.test.js
+++ TDD/test/geral.test.js
@@ -2,15 +2,19 @@ let app = require("../src/app")
 let supertest = require("supertest")
 let request = supertest(app)
 
-test("A aplicação deve responder na porta 31311", () => {
-    return request.get("/").then(res => expect(res.statusCode).toEqual(200))
-    //Nesse caso sempre deve ter um return em uma promise se não, não dá certo
+describe("Teste da porta", () => {
+    test("A aplicação deve responder na porta 31311", () => {
+        return request.get("/").then(res => expect(res.statusCode).toEqual(200))
+        //Nesse caso sempre deve ter um return em uma promise se não, não dá certo
+    })
 })
 
-test("Deve retornar vermelho como a cor favorita de lucas", () => {
-    return request.get("/cor/lucas").then(res => {
-        expect(res.statusCode).toEqual(200)
-        expect(res.body.cor).toEqual("vermelha")
-        expect(res.body.color).toEqual("red")
+describe("Teste das cores", () => {
+    test("Deve retornar vermelho como a cor favorita de lucas", () => {
+        return request.get("/cor/lucas").then(res => {
+            expect(res.statusCode).toEqual(200)
+            expect(res.body.cor).toEqual("vermelha")
+            expect(res.body.color).toEqual("red")
+        })
     })
 })
